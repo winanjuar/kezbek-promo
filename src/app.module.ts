d1f871b34f86e7273278e220994b0011c124ea79
Sugeng Winanjuar <winanjuar@gmail.com>
2023-01-05 00:36:19 +0700

    cleansing dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
+import { RedisModule } from '@liaoliaots/nestjs-redis';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    RedisModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        config: {
+          host: configService.get<string>('REDIS_HOST'),
+          port: configService.get<number>('REDIS_PORT'),
+        },
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
